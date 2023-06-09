70572f1eccaff5873bee682a7e1746c4242ec7ed
Emil Gulayev <116492842+emil-seraphic@users.noreply.github.com>
2023-06-09 12:35:10 +0300

    use cluster module init

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express')
+const { Mutex } = require('async-mutex')
+const cluster = require('cluster')
+const os = require('os')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const cpus = os.cpus
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const numCPUs = cpus().length
+
+const inMemoryStoredWords = {}
+
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`)
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`)
+  })
+} else {
+  app.post('/words', (req, res) => {
+    const incomingWords = req.body.words.split(',')
+    res.send('Hello World!')
+  })
+
+  app.listen(port, () => {
+    console.log(`Worker ${process.pid} started`)
+    console.log(`Example app listening on port ${port}`)
+  })
+}
