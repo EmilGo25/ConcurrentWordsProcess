1a534eeb848e8b1f57f860ba4e12856a4fec8cd8
Emil Gulayev <116492842+emil-seraphic@users.noreply.github.com>
2023-06-09 13:31:15 +0300

    initial skeleton of logic

--- app.js
+++ app.js
@@ -4,27 +4,50 @@ const cluster = require('cluster')
 const os = require('os')
 const app = express()
 const port = 3000
-
 const cpus = os.cpus
-
 const numCPUs = cpus().length
 
 const inMemoryStoredWords = {}
 
+const processStats = ({ statsObject }) => {
+  return {}
+}
+
+// use multi-processing
 if (cluster.isPrimary) {
   console.log(`Primary ${process.pid} is running`)
-  // Fork workers.
   for (let i = 0; i < numCPUs; i++) {
-    cluster.fork()
+    const worker = cluster.fork()
+
+    worker.on('message', (wordsObject) => {
+      processStats({ statsObject: wordsObject })
+    })
   }
 
   cluster.on('exit', (worker, code, signal) => {
     console.log(`worker ${worker.process.pid} died`)
   })
-} else {
+
   app.post('/words', (req, res) => {
     const incomingWords = req.body.words.split(',')
-    res.send('Hello World!')
+    res.send('Words were integrated')
+  })
+
+  app.post('/stats', (req, res) => {
+    const stats = {}
+
+    res.send(stats)
+  })
+} else {
+  process.on('message', (message) => {
+    if (message === 'SEND_STATS') {
+      process.send(inMemoryStoredWords)
+    }
+  })
+
+  app.post('/words', (req, res) => {
+    const incomingWords = req.body.words.split(',')
+    res.send('Words were integrated')
   })
 
   app.listen(port, () => {
